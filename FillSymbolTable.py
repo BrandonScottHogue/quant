50d0bee6217d4baebeda2de09700ae18448bd22c
cameroncarroll123 <33472472+cameroncarroll123@users.noreply.github.com>
2017-11-11 09:23:49 -0600

    Add files via upload

--- FillSymbolTable.py
+++ FillSymbolTable.py
@@ -1,112 +1,65 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Aug 12 23:11:20 2017
+Created on Wed Nov  8 21:14:59 2017
 
 @author: Cameron
 """
 import os
-import csv
+import pandas as pd
 import numpy as np
-import datetime
-import MySQLdb as mdb
-from math import ceil
+from sqlalchemy import create_engine
+from datetime import datetime
 
-def convCsv(indexDir):
-    with open(indexDir, newline='',encoding='utf-8') as f:
-        reader = csv.reader(f)
-        data = list(reader)
-        return np.array(data)
+data = pd.DataFrame()
+# Get exchange info from csv and store in dataframe data
+indexes = ["NASDAQ","NYSE","AMEX"]
+for idx in indexes:
+  indexDir = os.getcwd() + "/Indexes/" + idx + ".csv"
+  df = pd.read_csv(indexDir)
+  # Fix info from csv
+  df = df.replace('na',np.nan)
+  df = df.drop("Unnamed: 8",axis=1)
+  df = df.drop('LastSale',axis=1)
+  df = df.drop('MarketCap',axis=1)
+  df = df.drop('Summary Quote',axis=1)
+  df.insert(0,'Exchange', idx)
+  data = pd.concat([data,df],ignore_index=True)
 
-def makeData(index, etf):
-    indexDir = os.getcwd() + "/Indexes/" + index + ".csv"
-    print(indexDir)
-    data = convCsv(indexDir)
-    # Create numpy array from Nasdaq.csv
-    #data = np.genfromtxt(indexDir,delimiter=",",dtype=str)
-    stock = ["stock" for i in range(len(data))]
-    data[:,8] = stock
-    data[0,8] = "instrument"
+# Add columns to data  
+now = datetime.utcnow()  
+data['Instrument'] = 'stock'
+data['Date_created'] = now
+data['Date_last_updated'] = now
+colsData = data.columns.tolist()
 
-    # Create numpy array from ETFList.csv
-    indexDir = os.getcwd() + "/Indexes/" + etf + ".csv"
-    etfData = convCsv(indexDir)
+# Get ETF info from csv cross check with info in data and add only what does
+# not alread exist
+indexDir = os.getcwd() + "/Indexes/ETFList.csv"
+etf = pd.read_csv(indexDir)
 
-    # If symbol is in both arrays change instrument column from
-    # 'stock' to 'ETF'
-    idx = range(1,len(data))
-    for i in idx:
-        if data[i,0] in etfData[:,0]:
-            data[i,8] = "ETF"
-    return data
+colsEtf = etf.columns.tolist()
+etf = etf.drop(colsEtf[2:6],axis=1)
 
-def formatData(index,data):
-    now = datetime.datetime.utcnow()
-    length = len(data)-1
+etf['Instrument'] = 'ETF'
+etf['Date_created'] = now
+etf['Date_last_updated'] = now
 
-    indexCol = np.array([index for i in range(length)])
-    indexCol.shape = (length,1)
+# Changes Instrument to ETF if symbol appears in ETF info and drops rows in 
+# etf so info is not duplicated 
+data['Instrument'][data['Symbol'].isin(etf.index)] = 'ETF'
+etf.drop(etf.index[~etf['Symbol'].isin(data.index)])
+data = pd.concat([data,etf],ignore_index=True)
+data = data[colsData]
 
-    tickers = data[1:,0]
-    tickers.shape = (length,1)
-
-    names = data[1:,1]
-    names.shape = (length,1)
-
-    instrument = data[1:,8]
-    instrument.shape = (length,1)
-
-    sectors = data[1:,5]
-    sectors.shape = (length,1)
-
-    industry = data[1:,6]
-    industry.shape = (length,1)
-
-    ipoYear = data[1:,4]
-    ipoYear.shape = (length,1)
-
-    createdDate = [[str(now)] for i in range(length)]
-    lastUpDate = [[str(now)] for i in range(length)]
-
-    dbData = np.concatenate((indexCol,tickers,names,instrument,sectors,industry,ipoYear,createdDate,lastUpDate),axis=1)
-
-    print(dbData)
-    return dbData
-
-def updateSql(data):
-    data = tuple(map(tuple,data))
-
-    db_host = 'localhost'
-    db_user = 'serviceuser'
-    db_pass = 'longJNUG'
-    db_name = 'quantdb'
-    con = mdb.connect(host=db_host, user=db_user, passwd=db_pass, db=db_name)
-
-    cur = con.cursor()
-    try:
-        for i in range(0, int(ceil(len(data) / 100.0))):
-          cur.executemany(finalStr, data[i*100:(i+1)*100-1])
-    except mdb.DatabaseError:
-        con.rollback()
-        raise
-    else:
-        con.commit()
-    finally:
-        cur.close()
-
-indexes = ["NASDAQ","NYSE","AMEX","ETFList"]
-
-data1 = makeData(indexes[0],indexes[3])
-data1 = formatData(indexes[0],data1)
-data2 = makeData(indexes[1],indexes[3])
-data2 = formatData(indexes[1],data2)
-data3 = makeData(indexes[2],indexes[3])
-data3 = formatData(indexes[2],data3)
-data = [data1,data2,data3]
-
-columnStr = ("exchange, ticker, name, instrument, sector, industry, "
-                "ipo_year, created_date, last_update_date")
-insertStr = ("%s, " * 9)[:-2]
-finalStr = "INSERT INTO symbol (%s) VALUES (%s)" % (columnStr, insertStr)
-
-for i in range(3):
-    updateSql(data[i])
+# Create and fill symbol table 
+# DELETES INFORMATION IN TABLE IF TABLE ALREADY EXISTS
+db_host = 'localhost'
+db_user = 'Cam'
+db_pass = 'password'
+db_name = 'securities_master'
+uri = ("mysql+mysqldb://" + db_user + ":" + db_pass + 
+                       "@" + db_host + "/" + db_name)
+print(uri)
+engine = create_engine(uri)
+data.to_sql(con = engine, name='symbol', if_exists='replace')
+  
